b9796f63a24034c91e05e9e8aef4cab314ec8448
Gautam Kumar <g.k965069@gmail.com>
2025-01-29 22:11:11 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -39,10 +39,10 @@ app.use('/cars', carRoutes);
 
 
 if (process.env.NODE_ENV === "Production") {
-  app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
   });
 }
 
